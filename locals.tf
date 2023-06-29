02d66093897b9980faadab364d0b0dbbafbc50af
Eleazar Ponce <poncebe@gmail.com>
2023-06-29 15:32:36 -0600

    feat: Use one or more ARNs as targets for events when using existing SNS topics

--- locals.tf
+++ locals.tf
@@ -1,4 +1,5 @@
 locals {
+  // Get the sub levels to monitor given a starting level
   severity_levels = [
     "LOW",
     "MEDIUM",
@@ -7,4 +8,12 @@ locals {
   ]
   starting_index      = index(local.severity_levels, var.image_severity_level)
   sub_severity_levels = slice(local.severity_levels, local.starting_index, length(local.severity_levels))
+
+  // Define event_targets map from the cartesian product of severity levels and ARNs.
+  event_targets = {
+    for pair in setproduct(var.sns_topics_arns, local.sub_severity_levels) : "${pair[0]}.${pair[1]}" => {
+      arn = pair[0]
+      level = pair[1]
+    }
+  }
 }
